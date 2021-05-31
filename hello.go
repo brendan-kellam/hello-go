1cbcfee9598287cedf9d21ecf7e5beb42d864934
brendan-kellam <bshizzle1234@gmail.com>
2021-05-31 11:54:01 -0400

    Added morestrings package

--- hello.go
+++ hello.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/brendan-kellam/hello-go/morestrings"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
