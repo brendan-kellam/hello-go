f8229a00ae310ae0b6375aee563f4508b8ecd4bd
brendan-kellam <bshizzle1234@gmail.com>
2021-05-31 15:19:04 -0400

    Added functionality to download pdfs from 551 course webpage

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/brendan-kellam/hello-go
 
 go 1.16
 
-require github.com/google/go-cmp v0.5.6
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/google/go-cmp v0.5.6
+)
--- hello.go
+++ hello.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
-	"github.com/brendan-kellam/hello-go/morestrings"
-	"github.com/google/go-cmp/cmp"
+	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	c := colly.NewCollector()
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		if strings.Contains(link, ".pdf") && strings.Contains(link, "siamak.page") {
+			c.Visit(link)
+		}
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnResponse(func(r* colly.Response) {
+		link := r.Request.URL.String()
+		fmt.Printf("Link: %s\n", link)
+		if strings.Contains(link, ".pdf") {
+			fmt.Println("PDF found! ", r.Request.URL)
+			s := strings.Split(link, "/")
+			name := s[len(s)-1]
+			r.Save(name)
+		}
+	})
+
+	c.Visit("https://www.siamak.page/courses/COMP551F20/index.html")
 }
